d6a84f57af44aeee494a59cc9714a7f6c127ab41
Raja Kavalam <raja.kavalam@digitas.com>
2020-04-29 12:21:35 +0100

    Improvement

--- DeliverySlotFinder.js
+++ DeliverySlotFinder.js
@@ -171,7 +171,7 @@ if(!document.domain.match(validDomains)){
     console.log(`Delivery Slot Finder ${version} loaded`);
 }
 
-if(!document.getElementByXPath && thisSettings.enabled){
+if(thisSettings.enabled){
     document.getElementByXPath = function(sValue) { var a = this.evaluate(sValue, this, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null); if (a.snapshotLength > 0) { return a.snapshotItem(0); } };
     document.getElementsByXPath = function(sValue){ var aResult = new Array();var a = this.evaluate(sValue, this, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);for ( var i = 0 ; i < a.snapshotLength ; i++ ){aResult.push(a.snapshotItem(i));}return aResult;};
 
