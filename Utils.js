cb36dd330f5deec51095cd2a3453f320bdbf3895
Raja Kavalam <raja.kavalam@digitas.com>
2020-04-29 12:49:51 +0100

    Adding Utils.js

---
+++ Utils.js
@@ -0,0 +1,4 @@
+console.log("Script Loader - Utils.js getting loaded")
+document.getElementByXPath = function(sValue) { var a = this.evaluate(sValue, this, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null); if (a.snapshotLength > 0) { return a.snapshotItem(0); } };
+document.getElementsByXPath = function(sValue){ var aResult = new Array();var a = this.evaluate(sValue, this, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);for ( var i = 0 ; i < a.snapshotLength ; i++ ){aResult.push(a.snapshotItem(i));}return aResult;};
+
